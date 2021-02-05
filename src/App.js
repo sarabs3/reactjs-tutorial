36aafeae793d1be30df86021ef8a5aa30d209d3c
sarabjeet singh <sarabsingh212@gmail.com>
2021-02-05 16:47:37 +0530

    Integrated new forms

--- src/App.js
+++ src/App.js
@@ -2,71 +2,67 @@ import React, {useState} from 'react';
 import './App.css';
 import Table from './components/Table';
 import ViewTrade from './components/ViewTrade';
+import ProfitBox from "./components/ProfitBox/ProfitBox";
+import SellTradeForm from "./components/SellTradeForm/SellTradeForm";
+import AddPurchaseForm from "./components/AddPurchaseForm/AddPurchaseForm";
 
 const App = () => {
 
   const [view, updateView]  = useState(0);
   const [records, updateRecord] = useState([]);
+  const [sellTradeForm, updateSellTradeForm] = useState(false);
+  const [selectedTrade, updateSelectedTrade] = useState({});
   const handleFormSubmit = (e) => {
-
-    e.preventDefault();
-    const record = {};
+    const record = {...e};
     record.id = records.length + 1;
-    record.shareName = e.target[1].value;
-    record.action = e.target[2].value;
-    record.amount = e.target[3].value;
-    record.quantity = e.target[4].value;
     record.date = new Date();
-    console.log(record);
     records.push(record);
     updateRecord([...records]);
-
-
   };
+
+  // Sell previous purchased share
+  const sellShare = (id) => {
+    const getTrade = records.find((record) => record.id === id);
+    if (!getTrade) return;
+    updateSellTradeForm(true)
+    updateSelectedTrade({...getTrade});
+  }
+
+  // sell trade form update
+  const handleSellTrade = (record) => {
+
+    if (!selectedTrade) return ;
+
+    selectedTrade.action = "Traded";
+    selectedTrade.profit = record.amount - selectedTrade.amount;
+    records[selectedTrade.id - 1] = {...selectedTrade};
+    updateRecord([...records]);
+    updateSellTradeForm(false);
+  }
   return (
       <div className="App">
         <header className="App-header">
-          <button onClick={() => updateView(2)}>Add New Data</button>
-          {view === 0 && <Table view={() => updateView(1)} />}
+          {view !== 2 && <button onClick={() => updateView(2)}>Add New Data</button>}
+          {view === 0 && (
+              <>
+                <ProfitBox profit="7900.43" />
+              <Table view={() => updateView(1)} />
+              </>)}
           {view === 1 && (<div>
+            <ProfitBox />
                 <button onClick={() => updateView(0)}>Go Back</button>
                 <ViewTrade />
               </div>)}
           {view === 2 && (<div>
-            <form onSubmit={handleFormSubmit}>
-              <div>
-                <button onClick={() => updateView(0)}>Go Back</button>
-              </div>
-              <input
-                  type="text"
-                  placeholder="Share Name"
-                  name="shareName"
-                  className="inputField"
-              />
-              <select
-                  name="action"
-                  className="inputField"
-              >
-                <option value="buy">Buy</option>
-                <option value="sell">Sell</option>
-              </select>
-              <input
-                  type="text"
-                  placeholder="Amount"
-                  name="amount"
-                  className="inputField"
-              />
-              <input
-                  type="text"
-                  placeholder="Quantity"
-                  name="quantity"
-                  className="inputField"
-              />
-              <button type="submit">Submit</button>
-              <div>
-                <Table view={() => updateView(1)} data={records} />
-              </div>
-            </form>
+
+            <div>
+              <button onClick={() => updateView(0)}>Go Back</button>
+            </div>
+            {sellTradeForm && <SellTradeForm submit={handleSellTrade} />}
+            <AddPurchaseForm onSubmit={handleFormSubmit} />
+            <div>
+              <Table view={() => updateView(1)} data={records} sellShare={sellShare} />
+            </div>
           </div>)}
         </header>
       </div>
